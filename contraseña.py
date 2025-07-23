ffea6181dee0e58f40ceaabaa4770160f6e3adb2
Ian <supergachaworld4@gmail.com>
2025-07-23 09:43:00 -0300

    Actualizar contraseña.py

--- contraseña.py
+++ contraseña.py
@@ -1,10 +1,16 @@
 c = "1234"
 u = ""
 
-while u != c:
+while u != c and contador < 5:
     u = input("contraseña: ")
-    
+    contador = 0
     if u != c:
         print("incorrecta")
-        
-print("correcta")
+        contador = contador + 1
+        if contador = 5:
+           contador = "no"
+           break
+else:
+    print("correcta")     
+if contador == "no":
+    print("intenta mas tarde")
